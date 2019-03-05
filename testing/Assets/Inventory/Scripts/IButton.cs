d7f0539591c7fc79611e9df549dd94798daa935f
alecmck <arm007942@gmail.com>
2019-03-05 11:54:04 -0500

    TuesdayBuild

---
+++ testing/Assets/Inventory/Scripts/IButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IButton : MonoBehaviour
+{
+    public Text t;
+    private GameControl control;
+    private string Name;
+    public void SetUp(InventoryItem i, GameControl c)
+    {
+        t.text = i.GetName();
+        Name = i.GetName();
+        control = c;
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("MouseDrag");
+        control.Use(Name);
+        this.gameObject.SetActive(false);
+    }
+
+    public void Use()
+    {
+        control.Use(Name);
+        this.gameObject.SetActive(false);
+    }
+}

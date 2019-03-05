b178761ae7817410cb7d4f780f883771e6119a65
megjn97 <34458838+megjn97@users.noreply.github.com>
2019-03-04 23:25:57 -0500

    Adding Inventory System
    Basic UI. Items can be added and removed from inventory. When item in menu clicked, it appears somewhere in the world.

---
+++ testing/Assets/Inventory/Scripts/GameControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+               
+    public static GameControl control;
+    public static GameObject player;
+    public Transform ContenPanel;
+    public GameObject InventoryButton;
+    //public Text items;
+    // private InventoryItem[] inventory = new InventoryItem[10];
+    private Dictionary<string, InventoryItem> Inventory = new Dictionary<string, InventoryItem>();
+    //public GameObject canvas;
+    //public GameObject itemEntry;
+    
+	void Awake () {//This keeps the game control around if the scene is change. Currently does not work yet for changing scenes. 
+        if(control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+
+    public void Collect(InventoryItem o) //Called by InventoryItem.cs
+    {
+        Debug.Log("Collect");
+        Inventory.Add(o.GetName(), o);
+        Debug.Log("Added to Inventory");
+        GameObject button = (GameObject)GameObject.Instantiate(InventoryButton);
+        Debug.Log("Trying to set up button");
+        button.GetComponent<IButton>().SetUp(o, this);
+        Debug.Log("trying to add button");
+        button.transform.SetParent(ContenPanel);
+        Debug.Log("Should have added button");
+     }
+
+    public void Use(string o)
+    {
+        InventoryItem i = Inventory[o];
+        i.Remove(Camera.main.ScreenToWorldPoint(Input.mousePosition), new Quaternion(0,0,0,0));
+        Inventory.Remove(o);
+    }
+
+	void Update () {
+		
+	}
+}
+ 
---
+++ testing/Assets/Inventory/Scripts/InventoryItemSimple.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItemSimple : MonoBehaviour,InventoryItem {
+
+    public string itemName;
+    private bool inInventory;
+    public GameControl control;
+
+    public void Enter()
+    {
+        inInventory = true;
+        control.Collect(this);
+        this.gameObject.SetActive(false);
+        
+    }
+
+    public string GetName()
+    {
+        return itemName;
+    }
+
+    public bool InInventory()
+    {
+        return inInventory;
+    }
+
+    // Use this for initialization
+    void Start () {
+        inInventory = false;
+	}
+
+    public void Remove(Vector3 position, Quaternion rotation)
+    {
+        inInventory = false;
+        this.gameObject.SetActive(true);
+        this.gameObject.transform.SetPositionAndRotation(position, rotation);
+    }
+
+    private void OnMouseDrag()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Enter();
+        }
+    }
+}
